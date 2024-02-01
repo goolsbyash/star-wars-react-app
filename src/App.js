b4707dc545c930ea18c4ec4931d3ec7370901dfc
goolsbyash <goolsbyashley015@gmail.com>
2024-02-01 14:45:00 -0500

    Added fetchData to retrieve data from API and added routes.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header.js";
+import Main from "./pages/Main.js";
 
 function App() {
+  const [starships, setStarships] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://swapi.dev/api/starships/");
+        const data = await res.json();
+        const starshipData = data.results;
+        console.log(starshipData);
+
+        setStarships(starshipData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <h1>Star Wars React App</h1>
+      <center>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Main starships={starships} />} />
+          {/* <Route path="/starship/:name" element={<Main starships={starships}/>} /> */}
+        </Routes>
+      </center>
     </div>
   );
 }
