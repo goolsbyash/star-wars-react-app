29ea07c1a9b4b8911b6ba0217f6ef000e1c2b04a
goolsbyash <goolsbyashley015@gmail.com>
2024-02-01 14:31:15 -0500

    Added Header component to include link to return to main page.

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+
+export default function Header() {
+    return(
+        <>
+        <h5><Link to='/'>Star Wars React App</Link></h5>
+        </>
+    )
+}
